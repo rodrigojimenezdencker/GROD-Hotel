8bcb9b1e089d027643ede43525e2b0502d64ec4f
Rodrigo Fernando Jiménez Dencker <rjimenez@pasiona.com>
2020-01-13 23:34:58 +0100

    added more things to gulp

--- private/npm/gulpfile.js
+++ private/npm/gulpfile.js
@@ -5,34 +5,76 @@ var
 	concat = require('gulp-concat'),
 	rename = require('gulp-rename'),
 	uglifyCss = require('gulp-csso')
-;
+	;
 
-function css(done)
-{
+function css(done) {
 	gulp
 		.src([
 			'../shared/css/layout.css',
-			'../shared/css/style_guide.css'
+			'../shared/css/style_guide.css',
+			'../page/about/css/style.css',
+			'../page/booking/css/style.css',
+			'../page/bookingCompleted/css/style.css',
+			'../page/contact/css/style.css',
+			'../page/cookieUsage/css/style.css',
+			'../page/hotel/css/style.css',
+			'../page/index/css/style.css',
+			'../page/personalDataUsage/css/style.css',
+			'../page/search/css/style.css',
+			'../page/thanksForContactingUs/css/style.css'
 		])
 		.pipe(concat('style.css'))
 		.pipe(gulp.dest('../../public/css'))
 		.pipe(rename('style.min.css'))
 		.pipe(uglifyCss())
 		.pipe(gulp.dest('../../public/css'))
-	;
+		;
 
 	done();
 }
 
-function img(done)
-{
+function font(done) {
 	gulp
 		.src([
-			'../shared/img*'
+			'../shared/fonts**'
 		])
-		.pipe(gulp.dest('../../public/img'))
-	;
+		.pipe(gulp.dest('../../public/fonts'))
+		;
 
 	done();
 }
-exports.default = gulp.parallel(css, img);
+
+function img(done) {
+	gulp
+		.src([
+			'../shared/img**'
+		])
+		.pipe(gulp.dest('../../public/img'))
+		;
+
+	done();
+}
+
+function svg(done) {
+	gulp
+		.src([
+			'../shared/svg**'
+		])
+		.pipe(gulp.dest('../../public/svg'))
+		;
+
+	done();
+}
+
+function video(done) {
+	gulp
+		.src([
+			'../shared/svg**'
+		])
+		.pipe(gulp.dest('../../public/svg'))
+		;
+
+	done();
+}
+
+exports.default = gulp.parallel(css, font, img, svg, video);
