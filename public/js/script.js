79e7ee048cdc98ff80433ea99e8c93721af86cd1
Rodrigo Fernando Jiménez Dencker <rjimenez@pasiona.com>
2020-01-27 20:21:38 +0100

    Gulpfile done again

--- public/js/script.js
+++ public/js/script.js
@@ -36,6 +36,8 @@ setuppers['personal-data-usage'] = function () {
 }
 setuppers['room'] = function () {
     console.log('Room');
+    var widget = document.querySelector('[data-widget="room_form"]');
+    widget.addEventListener("submit", function(event) { event.preventDefault(); validateForm(widget); });
 }
 setuppers['search'] = function () {
     console.log('Search');
