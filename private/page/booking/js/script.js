d4ab0623d7b442953204dff9cd0bcbe40b94f3c7
Rodrigo Fernando Jiménez Dencker <rjimenez@pasiona.com>
2020-01-27 19:54:27 +0100

    Deleted async

--- private/page/booking/js/script.js
+++ private/page/booking/js/script.js
@@ -1,4 +1,4 @@
-setuppers['booking'] = async function () {
+setuppers['booking'] = function () {
     console.log("Booking");
     var widget = document.querySelector('[data-widget="booking_form"]');
     widget.addEventListener("submit", function(event) { event.preventDefault(); validateForm(widget); });
